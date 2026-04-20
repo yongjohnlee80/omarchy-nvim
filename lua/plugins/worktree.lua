56cf870885c639eba50bba655b0d6777fb8013c1
johno <yongjohnlee80@outlook.com>
2026-04-20 03:01:53 -0700

    updating worktree.nvim plugin to v0.2.0

--- lua/plugins/worktree.lua
+++ lua/plugins/worktree.lua
@@ -4,12 +4,15 @@ return {
     event = "VeryLazy",
     opts = {
       lsp_servers_to_restart = { "gopls" },
+      bare_dir = ".git", -- match existing repos cloned with `git clone --bare <url> .git`
     },
     keys = {
       { "<leader>gw", function() require("worktree").pick() end, desc = "Worktree: switch" },
       { "<leader>gW", function() require("worktree").home() end, desc = "Worktree: back to root" },
       { "<leader>gA", function() require("worktree").add() end, desc = "Worktree: add" },
       { "<leader>gR", function() require("worktree").remove() end, desc = "Worktree: remove" },
+      { "<leader>gC", function() require("worktree").clone() end, desc = "Worktree: clone" },
+      { "<leader>gc", function() require("worktree").init() end, desc = "Worktree: init new project" },
     },
   },
 }
